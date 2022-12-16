b13999f29cfebdeab92346c8d3caccefc20d8567
jyothikommineni1 <120538038+jyothikommineni1@users.noreply.github.com>
2022-12-16 14:29:27 -0500

    stringoperators
    assignment

---
+++ src/main/java/com/sira/learning/javalearningdec22/jyothi/operation.java
@@ -0,0 +1,15 @@
+package com.sira.learning.javalearningdec22.jyothi;
+
+public class operation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           int value=10;
+           int marks=value;
+           System.out.println("marks=" +marks);
+           marks+=3;
+           System.out.println(marks);
+	}
+	
+	
+		
---
+++ src/main/java/com/sira/learning/javalearningdec22/jyothi/stringoperators.java
@@ -0,0 +1,53 @@
+package com.sira.learning.javalearningdec22.jyothi;
+
+
+public class stringoperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String fname="  yashsush   ";
+		String lname="wonth";
+		System.out.println(fname+lname);
+		System.out.println("respons from concat method  " + fname.concat(lname).toUpperCase());
+		
+		
+		System.out.println("respons from concat method  " + fname.concat(lname).toLowerCase());
+		System.out.println(fname.charAt(3));
+		System.out.println(fname.length());
+		System.out.println(fname.codePointAt(3));
+		//equals
+		boolean result= fname.equals("yash");
+		System.out.println(result);
+		//trim
+		  String result1 = fname.trim();
+		  System.out.println(result1);
+		  //replace
+		  String result2=fname.replace('a','d' );
+		  System.out.println(result2);
+		 //empty 
+		  boolean result3=fname.isEmpty();
+		  System.out.println(result3);
+		  //charat
+		  
+		  System.out.println(fname.charAt(2));
+		  
+		  //contains
+		  
+		  System.out.println(fname.contains(lname));
+		  
+		  //indexof
+		  
+		  System.out.println(fname.indexOf("s"));
+		  //substring
+		 System.out.println(fname.substring(6,8));
+		 //repeat
+		 System.out.println(fname.repeat(2));
+		 
+		 ;
+		 
+		 
+	}
+	
+
+}
